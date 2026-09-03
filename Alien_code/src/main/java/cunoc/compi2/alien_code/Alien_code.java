097e6323cdda1c5283b78a7514950dfe4d1040cf
AnthonyGup <133796691+AnthonyGup@users.noreply.github.com>
2026-09-03 11:47:09 -0600

    primer commit

---
+++ Alien_code/src/main/java/cunoc/compi2/alien_code/Alien_code.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cunoc.compi2.alien_code;
+
+/**
+ *
+ * @author Anthony
+ */
+public class Alien_code {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
